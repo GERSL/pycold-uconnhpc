8009fd0ede25983c571f2a97913a07a5cc64cc69
SuYe99 <remotesensingsuy@gmail.com>
2022-07-27 15:17:55 -0400

    modify hls script

--- submit_pycold_workflow_hls.sh
+++ submit_pycold_workflow_hls.sh
@@ -9,7 +9,7 @@
 ##SBATCH --share
 #SBATCH --ntasks=1
 #SBATCH --time=12:00:00                              # Job should run for up to 1.5 hours (for example)
-#SBATCH --array 1-100
+#SBATCH --array 1-200
 #SBATCH --mail-type=END                              # Event(s) that triggers email notification (BEGIN,END,FAIL,ALL)
 ##SBATCH --exclude=cn[66-69,71-136,325-328,376]
 #SBATCH --exclude=cn355,cn406,cn217,cn373,cn67,cn87,cn376,cn68,cn116,cn338,cn330,cn398,cn346,cn364,cn368,cn450,cn448,cn354,cn392,cn343,cn400,cn403,cn455,cn448,cn451 
